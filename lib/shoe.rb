ae6b66c466c58d077afc93fb8b6279ad5ea29181
Duy Pham <ericpham19@outlook.com>
2022-09-19 22:21:32 +1000

    finished

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,36 @@
-# Make your shoe class here!
+class Shoe
+  attr_accessor :color, :size , :material, :condition
+  attr_reader :brand
+
+  def intialize(brand)
+    @brand= brand
+  end
+
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end
+  
+
+end
+
+
+it 'has a color' do
+  shoe.color = "red"
+  expect(shoe.color).to eq("red")
+end
+
+it 'has a size' do
+  shoe.size = 9.5
+  expect(shoe.size).to eq(9.5)
+end
+
+it 'has a material' do
+  shoe.material = "suede"
+  expect(shoe.material).to eq("suede")
+end
+
+it 'has a condition' do
+  shoe.condition = "tattered"
+  expect(shoe.condition).to eq("tattered")
+end
